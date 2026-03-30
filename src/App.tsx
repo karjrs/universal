1eac926704e0179ebd952a7d8fde8f75bc018666
karjrs <karol.jurcz@gmail.com>
2026-03-30 21:04:31 +0200

    aliases setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { OrbitControls } from "@react-three/drei"
+import { Planet } from "components/planet"
 import { useRef } from "react"
-import { Planet } from "./planet"
 
 function App() {
   const controlsRef = useRef(null)
 
   return (
     <>
-      <OrbitControls ref={controlsRef} autoRotate autoRotateSpeed={5} />
+      <OrbitControls ref={controlsRef} autoRotate autoRotateSpeed={20} />
       <Planet position={[-15, 0, 0]} size={[2, 32, 32]} />
       <Planet position={[0, 0, 0]} size={[3, 32, 32]} />
       <Planet position={[10, 0, 0]} size={[1, 32, 32]} />
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
 
-// https://vite.dev/config/
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import tsconfigPaths from "vite-tsconfig-paths"
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tsconfigPaths(), tailwindcss()],
 })
